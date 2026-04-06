fc01836e4f3bcfc514eee74c744e63d5e57ffc16
Janos Papp <janos@songpush.com>
2026-04-06 17:04:16 +0200

    feat(user-auth): database migration for user auth

---
+++ src/db/migrations/v0.0.2.sql
@@ -0,0 +1,18 @@
+DROP TABLE IF EXISTS dictionary;
+
+CREATE TABLE IF NOT EXISTS users (
+  id INTEGER PRIMARY KEY,
+  google_id TEXT NOT NULL UNIQUE,
+  email TEXT NOT NULL UNIQUE,
+  name TEXT NOT NULL,
+  inserted TEXT NOT NULL
+);
+
+CREATE TABLE IF NOT EXISTS dictionary (
+  user_id INTEGER NOT NULL,
+  english TEXT NOT NULL,
+  swedish TEXT NOT NULL,
+  inserted TEXT NOT NULL,
+  PRIMARY KEY(user_id, english),
+  FOREIGN KEY(user_id) REFERENCES users(id)
+);
